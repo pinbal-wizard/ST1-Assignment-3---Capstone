eace066eed517fca15e82a7aa50131fcd3ab70d3
Anthony Bebek <ante882005@gmail.com>
2024-10-11 00:02:00 +1100

    Added functions to remove fluff and null data from values

--- analysis.py
+++ analysis.py
@@ -7,7 +7,7 @@ Analysise the file dependent on data to clean and import data
 Data exploration at basic level
 Visual and statistic correlation analysis for selection of best features (Analyse)
 '''
-
+#28
 
 import data
 import pandas as pd
@@ -27,13 +27,35 @@ class Analysis():
             if name in ["Year", "Model", "Title", "Price", "Kilometres", "Location", "ColourExtInt", "Car/Suv"]:
                 print(len(set(coloum)), ": " + name)
                 continue
-            #print(set(coloum))
-            print(len(set(coloum)), ": " + name) 
+            print(set(coloum))
+            print(len(set(coloum)), ": " + name)
+
+    def analyseValue(self, column : str, value : any) -> int:
+        '''
+        Get the amount of rows that have the same value in the same column
+
+        Parameters:
+        column (str): The name of the column to analyze.
+        value (any): The value to count in the specified column.
+
+        Returns:
+            int: The count of rows with the specified value in the column.
+        '''
+        if column in self.df.columns:
+            count = (self.df[column] == value).sum()
+            return count
+        else:
+            print(f"Column '{column}' does not exist in data.")
+            return 0
+        
 
 
     def __init__(self, data) -> None:
         self.df = data.getData()
         self.displayData()
+        print("------ Random statitics ------")
+        print(f"Electric vehicles: {self.analyseValue("FuelConsumption", "0")}")
+        print("------------------------------")
         pass
     
     
--- data.py
+++ data.py
@@ -18,12 +18,45 @@ class Data():
         Cleans the CSV data by removing null values
         '''
         # Drop data thats 'N/A', 'Other' or '-'
-        for name, col in self.df.items():
-            mask = self.df[name] == "Other" 
-            self.df = self.df[~mask]
-            mask = self.df[name] == "-"
-            self.df = self.df[~mask]
-            self.df.dropna(inplace=True)
+        def removeNull(self) -> None:
+            '''
+            Remove values that are 'Other', '-' or NULL
+            '''
+            for name, col in self.df.items():
+                mask = self.df[name] == "Other" 
+                self.df = self.df[~mask]
+                mask = self.df[name] == "-"
+                self.df = self.df[~mask]
+                self.df.dropna(inplace=True)
+
+        def removeFluff(self) -> None:
+            '''
+            Remove unessecary fluff from columns like FuelConsumption
+            '''
+            # Engine category will be cleaned once we have a discussion on how these values are going to be used
+            #self.df['Engine'] = self.df['Engine'].str.replace(' cyl', '')
+            #self.df['Engine'] = self.df['Engine'].str.replace(' L', '')
+            
+
+            self.df['FuelConsumption'] = self.df['FuelConsumption'].str.replace(' L / 100 km', '')
+            self.df['FuelConsumption'] = self.df['FuelConsumption'].astype(float)
+
+            self.df['CylindersinEngine'] = self.df['CylindersinEngine'].str.replace('cyl', '')
+            # Electric vechicals have no cylinders
+            self.df['CylindersinEngine'] = self.df['CylindersinEngine'].str.replace(' L', '')
+            self.df['CylindersinEngine'] = self.df['CylindersinEngine'].astype(int)
+
+            self.df['Doors'] = self.df['Doors'].str.replace(' Doors', '')
+            self.df['Doors'] = self.df['Doors'].astype(int)
+
+            self.df['Seats'] = self.df['Seats'].str.replace(' Seats', '')
+            self.df['Seats'] = self.df['Seats'].astype(int)
+
+        removeNull(self)
+        removeFluff(self)
+            
+
+        
 
     def getData(self):
         return self.df
